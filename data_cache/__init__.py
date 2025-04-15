6d4856622b2cb2d63c90c868e61b030c709a06f5
Daniel Hjertholm <8713259+danhje@users.noreply.github.com>
2025-04-15 11:51:12 +0200

    Dynamic versioning

--- data_cache/__init__.py
+++ data_cache/__init__.py
@@ -1,3 +1,4 @@
 from data_cache.cache_tools import numpy_cache, pandas_cache, read_metadata  # noqa: F401
+import importlib.metadata
 
-VERSION = "0.1.6"
+__version__ = importlib.metadata.version(__name__)
